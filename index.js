b657493340e1db1f35f32fd72bb7b7fd499a2325
Lauren Slayman <laurenaslayman@gmail.com>
2023-03-02 14:55:27 -0700

    test 3

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function introduction(name) {
-    console.log(`Hi, my name is ${name}.`);
+    // console.log(`Hi, my name is ${name}.`);
     return(`Hi, my name is ${name}.`)
   }
-  introduction("Aki");
+ // introduction("Aki");
 
 function introductionWithLanguage (name, language) {
-    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+   // console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
     return(`Hi, my name is ${name} and I am learning to program in ${language}.`)
 }
-introductionWithLanguage("Aki", "Ember.js")
+// introductionWithLanguage("Aki", "Ember.js")
 
 function introductionWithLanguageOptional(name, language = "JavaScript") {
-    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+    // console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
     return(`Hi, my name is ${name} and I am learning to program in ${language}.`)
 }
-introductionWithLanguageOptional("Gracie", "JavaScript")
+// introductionWithLanguageOptional("Gracie", "JavaScript")
